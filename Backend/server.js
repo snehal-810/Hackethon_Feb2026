57f822f3c355c79a78940ddd0f30c1f121c9d40d
snehal-810 <snehal.jadhav@sunbeaminfo.com>
2026-02-21 10:24:43 +0530

    updated and working server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const cors = require('cors')
 
-//user defined modules
-
-
-//creating the express object
+// creating express app
 const app = express()
 
-//add the middlewares
+// middlewares
 app.use(cors())
-app.use(express.static('images'))
 app.use(express.json())
-app.use(authorization)
+app.use(express.static('images'))
 
+// test route
+app.get('/', (req, res) => {
+  res.send('Admission Management Backend Running')
+})
 
-
-//startig the server at port 4000
-app.listen(4000, 'localhost', () => {
-  console.log('server started at port 4000')
+// start server
+app.listen(4000, () => {
+  console.log('Server started at port 4000')
 })
